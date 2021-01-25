f23061670a1ee4672b2a2fc3b2ef3d8122ea00f5
Emre Can <emre@kassyazilim.com>
2021-01-26 00:36:36 +0300

    services

---
+++ src/services/api.service.js
@@ -0,0 +1,33 @@
+import { request } from "./httpClient";
+
+export function getUsers(user){
+    return request('/api/user',user)
+}
+
+export function getOnlineUsers(){
+    return request('/api/user/online')
+}
+
+export function getUserSessions(user){
+    return request('/api/sessions',user)
+}
+
+export function getTotalTimeSpent(){
+    return request('/api/statistics')
+}
+
+export function mostActiveUsers(){
+    return request('/api/mostactive')
+}
+
+export async function getUserSessionsAnalysis(user){
+    return request('/api/user/statistics',user);
+}
+
+export async function getUserLast24Hour(user){
+    return request('/api/user/last24',user);
+}
+
+export async function compareTagee(user){
+    return request('/api/user/compare/tagee',user);
+}
---
+++ src/services/httpClient.js
@@ -0,0 +1,24 @@
+const client = require('http');
+
+function request(url,body={}) {
+    const req = client.request({ 
+        path: url,
+        host: '192.168.1.35',
+        port: 9000,
+        method:'POST'
+    });
+    return new Promise(function(resolve){
+        let buffer = JSON.stringify(body);
+        req.end(buffer);
+
+        req.on('response',(response)=>{
+            let data = '';
+            response.on('data',(chunk)=> data+=chunk);
+            response.on('end',()=> { resolve(JSON.parse(data))})
+        })
+    })
+}
+
+module.exports = {
+    request
+}
