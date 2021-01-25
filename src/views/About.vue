17120ba25a299344cae3307cd59497bead446a3c
Emre Can <emre@kassyazilim.com>
2021-01-26 00:35:34 +0300

    views

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script>
+import { store,mapGetters } from "vuex";
+import { compareTagee } from "../services/api.service";
+export default {
+  name:"CompareTagee",
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+    
+  },
+  computed:{
+    ...mapGetters(['users'])
+  }
+}
+</script>
+
+<style>
+
+</style>
