ab0caac5505ebd38a0c3b628b769754b0e1aac54
frost768 <droidplus99@gmail.com>
2021-01-31 21:23:48 +0300

    TG logger and sqlite integration

--- tg.js
+++ tg.js
@@ -1,15 +1,23 @@
 const { MTProto } = require('@mtproto/core');
+const readline = require('readline');
+const { EventEmitter } = require('events');
+const logger = new EventEmitter(); 
+const { writeFileSync, existsSync, readFileSync, unlinkSync } = require('fs');
 const api_id = parseInt(process.env.TG_API_ID)
 const api_hash = process.env.TG_API_HASH;
 const phone_number = process.env.PHONE_NUMBER;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 const tg = new MTProto({
     api_id,
     api_hash,
-    test: true
 });
 
 const creds = {
-    phone_number: "9996613941",
+    phone_number,
     phone_code_hash: "blabla",
     phone_code: "11111"
 }
@@ -26,7 +34,7 @@ tg.updateInitConnectionParams({
     app_version: '10.0.0',
 });
 
-tg.setDefaultDc(1);
+tg.setDefaultDc(4);
 function sendCode() {
     return tg.call('auth.sendCode', options);
 }
@@ -35,15 +43,98 @@ function signIn() {
     return tg.call('auth.signIn', creds);
 }
 
-async function init() {
-    const { phone_code_hash } =  await sendCode(options);
-    creds.phone_code_hash = phone_code_hash;
-    const signedIn = await signIn(creds);
-    console.info((new Date()).toISOString(),"TG Logged In")
+function getContacts() {
+    return tg.call('contacts.getContacts');
+}
+async function sign() {
+    const signedIn = await signIn(creds);    
+    console.info((new Date()).toISOString(),"TG Logged In") 
     return signedIn;
 }
 
+function importAuthorization() {
+    return tg.call('auth.importAuthorization');
+}
+function exportAuthorization() {
+    return tg.call('auth.exportAuthorization');
+}
+
+let contacts = undefined;
+async function init() {
+    let res = undefined;
+    const { phone_code_hash } =  await sendCode(options);
+    creds.phone_code_hash = phone_code_hash;
+    rl.question('Code: ', async (code) => {
+        creds.phone_code = parseInt(code);
+        // writeFileSync('./data/tghash',JSON.stringify([phone_code_hash,code]));
+        sign().then(response =>{
+            res = response
+        }).catch(err=>{
+            console.log(err);
+        })
+    })
+    if(!existsSync('./data/tgContacts.json')){
+        contacts = await getContacts();
+        writeFileSync('./data/tgContacts.json',JSON.stringify(contacts.users,null,'\t'),{encoding:'utf-8'})
+    }
+    else {
+        contacts = JSON.parse(readFileSync('./data/tgContacts.json'));
+    }
+    return res;
+}
+
+tg.updates.on('updateShort', message => {
+    const { update } = message;
+    let json = {
+        id:"",
+        type:""
+    }
+    if(update._ === "updateUserStatus") {
+        const { user_id } = update;
+        let name = contacts.find(x => x.id == user_id).first_name;
+        let id = contacts.find(x => x.id == user_id).phone;
+        if(update.status._ === "userStatusOnline"){
+            json = {
+                id,
+                name,
+                type:"available"
+            }
+            logger.emit('user-presence-update',json)
+        }
+        else if(update.status._ === "userStatusOffline"){
+            json = {
+                id,
+                name,
+                type:"unavailable"
+            }
+            logger.emit('user-presence-update',json)
+        }
+    }
+})
+init().then(x=>{
+    console.log("OK");
+    console.log(x);
+})
+.catch(async(x)=>{
+    console.log("ERROR: ",x.error_message);
+    if(x.error_code == 400)
+    {
+        const { phone_code_hash } =  await sendCode(options);
+        creds.phone_code_hash = phone_code_hash;
+        rl.question('Code: ', async (code) => {
+            creds.phone_code = parseInt(code);
+            // writeFileSync('./data/tghash',JSON.stringify([phone_code_hash,code]));
+            sign()
+            .then(res=> console.log(res))
+            .catch(err=>{
+                console.log("ERROR 2: ",err.error_message);
+            })
+        })
+    }
+})
+
 module.exports = {
     tg,
-    init
+    init,
+    logger
 }
