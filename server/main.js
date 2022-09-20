0a270c5298b06ccee7e007ca033503da65c3b89d
frost768 <droidplus99@gmail.com>
2022-09-20 23:59:17 +0300

    baileys update

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
-const { logger, init }  = require('./tg.js');
-const { wa, sendRequest }  = require('./wa.js');
-const { checkTempJson, sessionLogger, tgSessionLogger }  = require('./utils.js');
+const { startSock } = require('./wa.js');
+const { checkTempJson, sessionLogger } = require('./utils.js');
+// const { logger, init }  = require('./tg.js');
 const { startBackup } = require('./services/DropboxBackup.js');
 
 // --------------- WEBSOCKET -------------------
@@ -12,9 +12,9 @@ wss.on('connection', (socket, req) => {
   a.on('send', (args) => {
     socket.send(JSON.stringify(args));
   });
-  
-  socket.on('close',()=>{
-      console.log('cikti')
+
+  socket.on('close', () => {
+    console.log('cikti')
   })
 });
 // ---------------------------------------------
@@ -22,23 +22,24 @@ wss.on('connection', (socket, req) => {
 let users = [];
 let tg_users = [];
 async function main() {
-   //init(async ()=>{
-    startBackup();
-    users = checkTempJson();
-    await wa.connect()
-    await sendRequest();
-  //})
-
-  wa.on('CB:Presence', json => {
-    users = sessionLogger(json[1], users);
+  startBackup();
+  users = checkTempJson();
+  startSock(update => {
+    const { id, presences } = update;
+    const json = {
+      id,
+      presences,
+      type: presences[id].lastKnownPresence
+    };
+    users = sessionLogger(json, users);
     a.emit('send', json[1]);
   });
 
-  logger.on('user-presence-update', json => {
-    tg_users = tgSessionLogger(json, tg_users);
-  })
+  // logger.on('user-presence-update', json => {
+  //   tg_users = tgSessionLogger(json, tg_users);
+  // })
 
-  
+
 }
 
 main().catch((x) => console.log(x));
--- server/wa.js
+++ server/wa.js
@@ -1,37 +1,62 @@
-const { WAConnection, ReconnectMode, MessageType } = require('@adiwajshing/baileys');
+const { useMultiFileAuthState, fetchLatestBaileysVersion, DisconnectReason } = require('@adiwajshing/baileys');
+const makeWASocket = require('@adiwajshing/baileys');
 const names = require('./data/names.json');
 const { writeFileSync } = require('fs');
-const wa = new WAConnection();
-wa.autoReconnect = ReconnectMode.off;
-wa.loadAuthInfo('./data/auth_info.json');
-wa.on('open', (auth) =>
-    writeFileSync('./data/auth_info.json', JSON.stringify(auth, null, '\t'))
-);
+const startSock = async (onPresenceUpdate) => {
+	const { state, saveCreds } = await useMultiFileAuthState('data/auth')
+	// fetch latest version of WA Web
+	const { version, isLatest } = await fetchLatestBaileysVersion()
+	const sock = makeWASocket.default({
+		version,
+		printQRInTerminal: true,
+		auth: state,
+	});
 
-wa.on('close', async (reason) => {
-    if (reason.reason === 'close' || 'lost') {
-      await wa.connect(); 
-      await sendRequest();
-    }
-    else console.log('oh no got disconnected: ' + reason.reason);
-});
+	sock.ev.process(
+		// events is a map for event name => event data
+		async (events) => {
+			// something about the connection changed
+			// maybe it closed, or we received all offline message or connection opened
+			if (events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+				if (connection === 'close') {
+					// reconnect if not logged out
+					if (lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+						startSock()
+					} else {
+						console.log('Connection closed. You are logged out.')
+					}
+				} else if (connection === 'open') {
+					sendRequest(sock);
+				}
+			}
 
+			if (events['creds.update']) {
+				await saveCreds()
+			}
 
-async function sendRequest() {
-    const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-    let requests = [];
-    names.forEach(user => {
-      wa.getProfilePicture(user.id + '@c.us')
-      .then(data => user.pp = data)
-      .catch(err => console.log())
-      
-      requests.push(timer(100).then(() => wa.requestPresenceUpdate(user.id + '@c.us')))
-    })
-    await Promise.all(requests).then(()=> writeFileSync('./data/names.json', JSON.stringify(names, null, '\t')))
+			if (events['presence.update']) {
+				const update = events['presence.update']
+				onPresenceUpdate(update)
+			}
+		})
+	return sock;
 }
 
 module.exports = {
-    wa,
-    sendRequest
-}
+	startSock
+};
 
+async function sendRequest(wa) {
+	const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+	let requests = [];
+	names.forEach(user => {
+		// wa.profilePictureUrl(user.id + '@g.us')
+		// .then(data => user.pp = data)
+		// .catch(err => console.log(err))
+
+		requests.push(timer(100).then(() => wa.presenceSubscribe(user.id + '@s.whatsapp.net')))
+	})
+	await Promise.all(requests).then(() => writeFileSync('./data/names.json', JSON.stringify(names, null, '\t')))
+}
